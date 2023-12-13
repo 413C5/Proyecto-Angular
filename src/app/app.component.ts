5b9389383d29f8dffd544686cddb4a2c72940b9c
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2023-12-13 12:43:16 -0600

    Captura de eventos
    Se hace uso de botones ligados a eventos para aumentar/decrementar un valor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,28 +12,15 @@ import { RouterOutlet } from '@angular/router';
 
 export class AppComponent {
   //Propiedades
-  nombre = 'Alejandro Zarate'
-  edad = 23
-  articulos = [
-    {
-      codigo: 1,
-      descripcion: 'naranjas',
-      precio: 540
-    },
-    {
-      codigo: 2,
-      descripcion: 'manzanas',
-      precio: 900
-    },
-    {
-      codigo: 3,
-      descripcion: 'peras',
-      precio: 490
-    }
-  ]
+  title='proyecto001'
+  contador=0
 
-  //Devuelve un valor aleatorio 
-  generarNumero(){
-    return Math.floor(Math.random()*3)+1
+  incrementar(){
+    this.contador++
   }
+
+  decrementar(){
+    this.contador--
+  }
+ 
 }
