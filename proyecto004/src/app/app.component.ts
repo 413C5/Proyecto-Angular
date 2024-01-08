1675b4ee7c210aa79b5738cff209fef48e1b0e47
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-01-08 11:52:24 -0600

    Components: Triggering events from the child component to the parent component.
    Cronometer was created to demonstrate the use of user defined events

--- proyecto004/src/app/app.component.ts
+++ proyecto004/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CronometroComponent } from './cronometro/cronometro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CronometroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'proyecto004';
+
+  mensaje: string = ''
+
+  actualizar(t: number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)'
+  }
+
 }
--- proyecto004/src/app/cronometro/cronometro.component.ts
+++ proyecto004/src/app/cronometro/cronometro.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'Cronometro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cronometro.component.html',
   styleUrl: './cronometro.component.css'
 })
+
 export class CronometroComponent {
+
+  segundo: number = 0
+  @Input() inicio: number = 0
+
+  //Syntax to define event
+  @Output() multiplo10 = new EventEmitter<number>()
+
+  ngOnInit() {
+    this.segundo = this.inicio
+    
+    //Every second a second is added and cheks if 10 secs have passed
+    setInterval(() => {
+      this.segundo++
+      if (this.segundo % 10 === 0)
+        this.multiplo10.emit(this.segundo)
+    }, 1000)
+  }
 
 }
