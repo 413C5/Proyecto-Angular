340cde6ef745240e100aeb4f197e889e2a4995e8
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-01-08 10:17:47 -0600

    Created component cronometro

---
+++ proyecto004/src/app/cronometro/cronometro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Cronometro',
+  standalone: true,
+  imports: [],
+  templateUrl: './cronometro.component.html',
+  styleUrl: './cronometro.component.css'
+})
+export class CronometroComponent {
+
+}
